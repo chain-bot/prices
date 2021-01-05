7e3e82aa4ac403cec79151a448553f8ffa80f18e
zahin <zahin.dev@gmail.com>
2021-01-04 21:30:50 -0500

    Uber FX DI

--- main/main.go
+++ main/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	_ "github.com/joho/godotenv/autoload"
+	app "github.com/mochahub/coinprice-scraper/main/app"
 	"go.uber.org/fx"
 	"log"
 )
@@ -9,7 +11,8 @@ import (
 func main() {
 	// TODO: Find a Better Logging Framework
 	log.Println("Scraper Config:")
-	//app.StartScraper()
-	app := fx.New()
-	_ = app.Start(context.Background())
+	// fx.provide the code for the influx connection
+	fxApp := fx.New(fx.Invoke(app.StartScraper))
+	_ = fxApp.Start(context.Background())
+	_ = fxApp.Done()
 }
