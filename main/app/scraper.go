8468752fd3343ed238f57d9b77fa63cbeca85774
zahin <zahin.dev@gmail.com>
2021-01-07 16:01:57 -0500

    Remove comment

--- main/app/scraper.go
+++ main/app/scraper.go
@@ -29,15 +29,6 @@ func StartScraper(
 		// TODO(Zahin): Handle Errors
 		go ScrapeExchange(ctx, secrets, db, influxDBClient, clients[i])
 	}
-	// TODO: Move Below Comments to a Space Document
-	// General Flow:
-	// - Loop Through Implemented Exchanges
-	// - Each Exchange is a goroutine
-	// - Each goroutine writes to influx independently (conflicts will take latest price?)
-	//	- Each exchange writes to their own bucket with expiry
-	//	- A separate bucket aggregates price data from exchange buckets periodically
-	// - TODO: Interface File that Each Exchange Inherits
-	//	- Implement Influx Config and Functions here to abstract from exchange
 	return nil
 }
 
