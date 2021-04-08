0c8878b1615fca97bb96170964b5394924a7edf1
Zahin Mohammad <zahin.dev@gmail.com>
2021-04-08 17:00:55 -0400

    Circleci project setup (#52)
    * Add .circleci/config.yml
    
    * Updated config.yml
    
    * Updated config.yml
    
    * Updated config.yml
    
    * Updated config.yml
    
    * Updated config.yml
    
    * Run tests with docker
    
    * Updated config.yml
    
    * update circleci config
    
    * Updated config.yml
    
    * Updated config.yml
    
    * update circleci dockerfile

--- scripts/update-code-climate-coverage.sh
+++ scripts/update-code-climate-coverage.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Requires cc-test-reporter: https://docs.codeclimate.com/docs/configuring-test-coverage#section-locations-of-pre-built-binaries
-source .env
-cc-test-reporter before-build
+# source .env
+./cc-test-reporter before-build
 cd app || exit
 go test -coverprofile ../c.out $(go list ./... | grep -v /psql/generated)
 cd ../
-cc-test-reporter format-coverage -t gocov --prefix github.com/mochahub/coinprice-scraper c.out
-cc-test-reporter after-build --prefix github.com/mochahub/coinprice-scraper -r=$CC_TEST_REPORTER_ID
-
+./cc-test-reporter format-coverage -t gocov --prefix github.com/mochahub/coinprice-scraper c.out
+./cc-test-reporter after-build --prefix github.com/mochahub/coinprice-scraper -r=$CC_TEST_REPORTER_ID
+go tool cover -html=c.out -o=coverage/c.html
 mv c.out coverage/c.out
