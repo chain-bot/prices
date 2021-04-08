3b90ed09167918a9dbe8dd7c1247b61845befe64
zahin <zahin.dev@gmail.com>
2021-04-08 16:36:24 -0400

    Run tests with docker

--- scripts/run-test-with-coverage.sh
+++ scripts/run-test-with-coverage.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-source ./.env
 cd app || exit
-#go test -v $(go list ./... | grep -v /psql/generated)
-go test -v $(go list ./... | grep -v /psql/generated) -v -coverprofile ../cover.out . fmt
+go test -coverprofile ../c.out $(go list ./... | grep -v /psql/generated)
 cd ../
-go tool cover -html=cover.out -o=cover.html
-go tool cover -func=cover.out -o=cover.out
-gobadge -filename=cover.out
-rm -rf cover.out
-mv cover.html docs/cover.html
+
+#source ./.env
+#cd app || exit
+##go test -v $(go list ./... | grep -v /psql/generated)
+#go test -v $(go list ./... | grep -v /psql/generated) -v -coverprofile ../cover.out . fmt
+#cd ../
+#go tool cover -html=cover.out -o=cover.html
+#go tool cover -func=cover.out -o=cover.out
+#gobadge -filename=cover.out
+#rm -rf cover.out
+#mv cover.html docs/cover.html
 #if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 #  xdg-open cover.html
 #elif [[ "$OSTYPE" == "darwin"* ]]; then
